dc72eb1feaaa4c00fd5439b62540d4ad77fef15e
Shalinga Manasinghe <shalingams@gmail.com>
2024-02-23 13:56:23 +0200

    Updating url to support V2

--- spec/omniauth/strategies/tiktok_spec.rb
+++ spec/omniauth/strategies/tiktok_spec.rb
@@ -9,15 +9,15 @@ describe OmniAuth::Strategies::Tiktok do
 
   context 'when fetch client options' do
     it 'have correct site' do
-      expect(subject.options.client_options.site).to eq('https://open-api.tiktok.com')
+      expect(subject.options.client_options.site).to eq('https://open.tiktokapis.com')
     end
 
     it 'have correct authorize url' do
-      expect(subject.options.client_options.authorize_url).to eq('https://open-api.tiktok.com/platform/oauth/connect')
+      expect(subject.options.client_options.authorize_url).to eq('https://open.tiktokapis.com/v2/oauth/connect')
     end
 
     it 'have correct token url' do
-      expect(subject.options.client_options.token_url).to eq('https://open-api.tiktok.com/oauth/access_token')
+      expect(subject.options.client_options.token_url).to eq('https://open.tiktokapis.com/v2/oauth/token/')
     end
   end
 end
